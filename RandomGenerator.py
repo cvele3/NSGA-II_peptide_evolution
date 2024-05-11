f0613d7597451e61376460b988cacb7ffa114227
cvele3 <cvetkojakov@gmail.com>
2024-05-11 15:40:12 +0200

    Added random generator

---
+++ RandomGenerator.py
@@ -0,0 +1,10 @@
+import random
+from Constants import AMINO_ACIDS
+
+def generate_random_peptides(lowerRange, upperRange, numberOfRandomlyGeneratedPeptides):
+    peptides = []
+    for _ in range(numberOfRandomlyGeneratedPeptides):
+        length = random.randint(lowerRange, upperRange)
+        peptide_sequence = random.choices(AMINO_ACIDS, k=length)
+        peptides.append(peptide_sequence)
+    return peptides
